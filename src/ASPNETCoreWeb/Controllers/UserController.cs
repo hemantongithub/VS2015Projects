9fa6da281af3d66a618283be3df6116fe40ef8d2
User <User@WINDEV1612EVAL>
2017-02-08 21:47:51 +0000

    test commit

---
+++ src/ASPNETCoreWeb/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNETCoreWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserCtrl(int ID=1)
+        {
+            ViewData["Message"] = "Hello Hemant";
+            ViewData["ID"] = ID;
+            return View();
+        }
+        public IActionResult Welcome(string name, int ID = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["ID"] = ID;
+            return View();
+        }
+    }
+}
---
+++ src/ASPNETCoreWeb/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreWeb.Models
+{
+    public class User
+    {
+        string firstName { get; set; }
+        string lastName { get; set; }
+        int age { get; set; }
+        string city { get; set; }
+
+    }
+}
